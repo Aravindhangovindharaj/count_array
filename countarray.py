e8413b9597313d9f6e36e8e7681bdf1c735a0c5c
Aravindhangovindharaj <139099776+Aravindhangovindharaj@users.noreply.github.com>
2023-07-17 07:49:29 -0700

    Add files via upload

---
+++ countarray.py
@@ -0,0 +1,13 @@
+n = list(int,input("num ="  ).split()))
+def count_smallNum(n):
+    le = len(n)
+    lst =[]
+    for i in range(0, le):
+        count=0
+        for j in range(i+n, le):
+            if 1[i]0 > 1[j]:
+               count +=n
+            
+               lst.append(count)
+               return lst
+            print(count_smallNum(n))
